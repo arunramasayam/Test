5a19ae5dd77e338bade6b397942f003541b1cb74
ramas <ramas@DESKTOP-SRTVUF0>
2023-02-17 20:18:54 +0530

    add

---
+++ Scripts/New Test Case/Script1676645262957.groovy
@@ -0,0 +1,17 @@
+import java.util.concurrent.TimeUnit
+
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+
+import com.kms.katalon.core.webui.driver.DriverFactory
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+
+
+WebUI.openBrowser('');
+WebDriver driver =DriverFactory.getWebDriver();
+driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+driver.manage().window().maximize();
+driver.get("https://atlasqa.cartus.com/");
+driver.findElement(By.id("UserID")).sendKeys("atlas441");
+driver.findElement(By.id("Passwd")).sendKeys("Q1Qa445566")
---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1d24fabc-7b09-435c-a2d9-831a66060417</testSuiteGuid>
+   <testCaseLink>
+      <guid>370f248d-e4ca-4f35-a263-2dd271ea546b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
